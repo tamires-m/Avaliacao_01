af86229999a87a0b03e968c425ac4e9a3d88b6ed
Tamires Maiolini <tamires.maiolini@alunos.unis.edu.br>
2023-09-25 11:50:39 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Avaliacao 01"
+include(":app")
